542347904114a1e121c7d0ce48efb7c2754d54e7
1Com <piordik@mail.ru>
2023-11-11 18:08:49 +0300

    Revert "Error"
    This reverts commit c0aad2f0624592f89f11cf364cf13a26ac645059.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        final int three = 3;
+        int a = Integer.valueOf(args[0]);
+        int b = Integer.valueOf(args[1]);
+        int c = Integer.valueOf(args[2]);
+        int d = Integer.valueOf(args[three]);
+        c *= b;
+        b *= d;
+        a = a * d + c;
+        final int ten = 10;
+        for (int i = 1; i < ten; ++i) {
+            if (a % i == 0 && b % i == 0) {
+                a /= i;
+                b /= i;
+            }
+        }
         System.out.println(a);
         System.out.println(b);
     }
